409e883697d82ff2441c1e7a3b9557c38cafbeed
vsey <vsey@gmx.net>
2025-10-03 12:01:11 +0200

    first commit

---
+++ base_model.py
@@ -0,0 +1,211 @@
+"""Create model of SPAwGBP instance before any local branching constraints."""
+
+import gurobipy as gp
+import pandas as pd
+from gurobipy import GRB
+
+type ProjectsInfo = pd.DataFrame
+type StudentsInfo = pd.DataFrame
+
+
+def base_model(
+    instance: tuple[ProjectsInfo, StudentsInfo],
+    reward_bilateral: int,
+    penalty_unassigned: int,
+) -> gp.Model:
+    """Return the basic model for the instance"""
+    projects, students = instance
+    model = gp.Model()
+
+    project_ids = range(len(projects))
+    student_ids = range(len(students))
+
+    group_ids = {
+        project_id: range(max_num_groups)
+        for project_id, max_num_groups in enumerate(projects["max#groups"])
+    }
+
+    project_group_pairs = [
+        (project_id, group_id)
+        for project_id, project_group_ids in group_ids.items()
+        for group_id in project_group_ids
+    ]
+
+    project_group_student_triples = [
+        (project_id, group_id, student_id)
+        for project_id, group_id in project_group_pairs
+        for student_id in student_ids
+    ]
+
+    assign_students = model.addVars(
+        project_group_student_triples,
+        vtype=GRB.BINARY,
+        name="assign_students",
+    )
+
+    establish_groups = model.addVars(
+        project_group_pairs,
+        vtype=GRB.BINARY,
+        name="establish_groups",
+    )
+
+    mutual_pairs = {
+        (student_id, partner_id)
+        for student_id, partner_ids in enumerate(students["fav_partners"])
+        for partner_id in partner_ids
+        if partner_id > student_id and student_id in students["fav_partners"][partner_id]
+    }
+
+    mutual_unrealized = model.addVars(mutual_pairs, vtype=GRB.BINARY, name="mutual_unrealized")
+
+    unassigned_students = model.addVars(student_ids, name="unassigned_students")
+
+    group_size_surplus = model.addVars(project_group_pairs, name="group_size_surplus")
+    group_size_deficit = model.addVars(project_group_pairs, name="group_size_deficit")
+
+    project_preferences = {
+        (student_id, project_id): preference_value
+        for student_id, preference_values in enumerate(students["project_prefs"])
+        for project_id, preference_value in enumerate(preference_values)
+    }
+
+    sum_realized_project_preferences = gp.quicksum(
+        project_preferences[student_id, project_id]
+        * assign_students[project_id, group_id, student_id]
+        for project_id, group_id, student_id in project_group_student_triples
+    )
+
+    sum_reward_bilateral = reward_bilateral * gp.quicksum(
+        1 - mutual_unrealized[*mutual_pair] for mutual_pair in mutual_pairs
+    )
+
+    sum_penalties_unassigned = penalty_unassigned * gp.quicksum(unassigned_students.values())
+
+    sum_penalties_surplus_groups = gp.quicksum(
+        penalty_surplus_group * establish_groups[project_id, group_id]
+        for project_id, penalty_surplus_group, num_groups_desired in zip(
+            project_ids, projects["pen_groups"], projects["desired#groups"]
+        )
+        for group_id in group_ids[project_id]
+        if group_id >= num_groups_desired
+    )
+
+    sum_penalties_group_size = gp.quicksum(
+        penalty_size_deviation
+        * (group_size_surplus[project_id, group_id] + group_size_deficit[project_id, group_id])
+        for project_id, penalty_size_deviation in enumerate(projects["pen_size"])
+        for group_id in group_ids[project_id]
+    )
+
+    model.setObjective(
+        sum_realized_project_preferences
+        + sum_reward_bilateral
+        - sum_penalties_unassigned
+        - sum_penalties_surplus_groups
+        - sum_penalties_group_size,
+        sense=GRB.MAXIMIZE,
+    )
+
+    model.addConstrs(
+        (
+            assign_students.sum("*", "*", student_id) + unassigned_students[student_id] == 1
+            for student_id in student_ids
+        ),
+        name="one_assignment_or_unassigned",
+    )
+
+    model.addConstrs(
+        (
+            establish_groups[project_id, group_id] <= establish_groups[project_id, group_id - 1]
+            for project_id, group_id in project_group_pairs
+            if group_id > 0
+        ),
+        name="open_groups_consecutively",
+    )
+
+    model.addConstrs(
+        (
+            assign_students.sum(project_id, group_id, "*")
+            >= min_group_size * establish_groups[project_id, group_id]
+            for project_id, min_group_size in enumerate(projects["min_group_size"])
+            for group_id in group_ids[project_id]
+        ),
+        name="min_group_size_if_open",
+    )
+
+    model.addConstrs(
+        (
+            assign_students.sum(project_id, group_id, "*")
+            <= max_group_size * establish_groups[project_id, group_id]
+            for project_id, max_group_size in enumerate(projects["max_group_size"])
+            for group_id in group_ids[project_id]
+        ),
+        name="max_group_size_if_open",
+    )
+
+    model.addConstrs(
+        (
+            group_size_surplus[project_id, group_id]
+            >= assign_students.sum(project_id, group_id, "*") - ideal_group_size
+            for project_id, ideal_group_size in enumerate(projects["ideal_group_size"])
+            for group_id in group_ids[project_id]
+        ),
+        name="lower_bound_group_size_surplus",
+    )
+
+    model.addConstrs(
+        (
+            group_size_deficit[project_id, group_id]
+            >= ideal_group_size
+            - assign_students.sum(project_id, group_id, "*")
+            - max_group_size * (1 - establish_groups[project_id, group_id])
+            for project_id, ideal_group_size, max_group_size in zip(
+                project_ids, projects["ideal_group_size"], projects["max_group_size"]
+            )
+            for group_id in group_ids[project_id]
+        ),
+        name="lower_bound_group_size_deficit",
+    )
+
+    max_num_groups = max(projects["max#groups"])
+
+    unique_group_identifiers = {
+        (project_id, group_id): project_id + group_id / max_num_groups
+        for project_id, group_id in project_group_pairs
+    }
+
+    num_projects = len(projects)
+
+    model.addConstrs(
+        (
+            mutual_unrealized[first, second] * num_projects
+            >= sum(
+                unique_group_identifiers[project_id, group_id]
+                * (
+                    assign_students[project_id, group_id, first]
+                    - assign_students[project_id, group_id, second]
+                )
+                for project_id, group_id in project_group_pairs
+            )
+            for first, second in mutual_pairs
+        ),
+        name="only_reward_materialized_pairs_1",
+    )
+
+    model.addConstrs(
+        (
+            mutual_unrealized[first, second] * num_projects
+            >= sum(
+                unique_group_identifiers[project_id, group_id]
+                * (
+                    assign_students[project_id, group_id, second]
+                    - assign_students[project_id, group_id, first]
+                )
+                for project_id, group_id in project_group_pairs
+            )
+            for first, second in mutual_pairs
+        ),
+        name="only_reward_materialized_pairs_1",
+    )
+
+    return model
---
+++ load_instance.py
@@ -0,0 +1,26 @@
+"""Functions for loading an instance."""
+
+import json
+from pathlib import Path
+
+import pandas as pd
+
+type ProjectsInfo = pd.DataFrame
+type StudentsInfo = pd.DataFrame
+
+
+def load_instance(
+    num_projects: int, num_students: int, instance_index: int
+) -> tuple[ProjectsInfo, StudentsInfo]:
+    """Return the specified instance of the SPAwGBP."""
+    folder = Path("instances")
+    subfolder = f"{num_projects}_projects_{num_students}_students"
+    shared_prefix = f"generic_{num_projects}_{num_students}"
+    shared_suffix = f"{instance_index}.csv"
+    path_projects = folder / subfolder / f"{shared_prefix}_projects_{shared_suffix}"
+    path_students = folder / subfolder / f"{shared_prefix}_students_{shared_suffix}"
+    projects: pd.DataFrame = pd.read_csv(path_projects)  # type: ignore
+    students: pd.DataFrame = pd.read_csv(path_students)  # type: ignore
+    students["fav_partners"] = students["fav_partners"].apply(lambda x: set(json.loads(x)))  # type: ignore
+    students["project_prefs"] = students["project_prefs"].apply(lambda x: tuple(json.loads(x)))  # type: ignore
+    return projects, students
