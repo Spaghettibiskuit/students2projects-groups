62cc337cb04830d34279ae859353ae23b3c45c5f
vsey <vsey@gmx.net>
2025-10-29 18:33:38 +0100

    marked a potential issue

--- branching_constraints_efficacy_checker.py
+++ branching_constraints_efficacy_checker.py
@@ -1,13 +1,14 @@
 class BranchingConstraintsEfficacyChecker:
 
     def __init__(self):
-        self.optimal_within_branching_constraints: dict[tuple[int | float, ...], bool] = {}
+        self.optimal_within_branching_constraints: dict[tuple[int, ...], bool] = {}
 
     def check(self, solution_variables: tuple[int | float, ...], is_optimal_within_bounds: bool):
         rounded = tuple(round(var) for var in solution_variables)
         if rounded in self.optimal_within_branching_constraints:
             was_optimal_within_bounds = self.optimal_within_branching_constraints[rounded]
 
+            # too strict in general case, works in particular case
             if was_optimal_within_bounds is False and is_optimal_within_bounds is False:
                 raise ValueError(
                     "Solution previously found as not optimal found again as not optimal!"
